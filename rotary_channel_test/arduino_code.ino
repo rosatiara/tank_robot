663651c7399d6bcbc0c58e23db58bb6ad22049dd
Rosa Tiara <rosatiarag@gmail.com>
2022-12-14 14:34:55 +0700

    Update arduino_code.ino

--- rotary_channel_test/arduino_code.ino
+++ rotary_channel_test/arduino_code.ino
@@ -2,23 +2,18 @@
 const int motorDirPinL = 5; // done
 const int motorPWMPinL = 6; // done
 const int enablePinL = 7;
+const int encoderPinAL = 2; // channel A & B motor kiri
+const int encoderPinBL = 3;
+const int powerPinL = A0;
 
 // motor kanan (kuning)
 const int motorDirPinR = 9; //
 const int motorPWMPinR = 10; //
-const int enablePinR = 8;//
-
-// channel A & B motor kiri
-const int encoderPinAL = 2; 
-const int encoderPinBL = 3;
-
-const int powerPinL = A0;
-const int powerPinR = A1;
-
-
-// channel A & B motor kanan
+const int enablePinR = 8; //
 const int encoderPinAR = 12;
 const int encoderPinBR = 13;
+const int powerPinR = A1;
+
 int encoderPos = 0;
 
 const float ratio = 360./188.611/48.;
@@ -26,49 +21,21 @@ const float ratio = 360./188.611/48.;
 float Kp = 30;
 float targetDeg = 360;
 
-void doEncoderAL() {  
-  encoderPos += (digitalRead(encoderPinAL)==digitalRead(encoderPinBL))?1:-1;
-}
-void doEncoderBL() {  
-  encoderPos += (digitalRead(encoderPinAL)==digitalRead(encoderPinBL))?-1:1;
-}
-
-void doEncoderAR() {
-  encoderPos += (digitalRead(encoderPinAR)==digitalRead(encoderPinBR))?1:-1;
-}
-
-void doEncoderBR(){
-  encoderPos += (digitalRead(encoderPinAR)==digitalRead(encoderPinBR))?-1:1;
-}
-
-void doMotor(bool dir, int vel) {
-  digitalWrite(motorDirPinL, dir);
-  analogWrite(motorPWMPinL, dir?(255 - vel):vel);
-  
-  digitalWrite(motorDirPinR, dir);
-  analogWrite(motorPWMPinR, dir?(255 - vel):vel);
-}
-
 void setup() {
   Serial.begin(1200);
 
   pinMode(powerPinL, OUTPUT);
   digitalWrite(powerPinL, HIGH);
-
-  
   pinMode(powerPinR, OUTPUT);
   digitalWrite(powerPinR, HIGH);
   
-  
   pinMode(encoderPinAL, INPUT_PULLUP);
   attachInterrupt(0, doEncoderAL, CHANGE);
-  
   pinMode(encoderPinBL, INPUT_PULLUP);
   attachInterrupt(1, doEncoderBL, CHANGE);
   
   pinMode(encoderPinAR, INPUT_PULLUP);
   attachInterrupt(0, doEncoderAR, CHANGE);
-  
   pinMode(encoderPinBR, INPUT_PULLUP);
   attachInterrupt(1, doEncoderBR, CHANGE);
   
@@ -94,4 +61,26 @@ void loop() {
   Serial.print(control);
   Serial.print("    motorVel : ");
   Serial.println(min(abs(control), 255)); 
+}
+
+void doEncoderAL() {  
+  encoderPos += (digitalRead(encoderPinAL)==digitalRead(encoderPinBL))?1:-1;
+}
+void doEncoderBL() {  
+  encoderPos += (digitalRead(encoderPinAL)==digitalRead(encoderPinBL))?-1:1;
+}
+
+void doEncoderAR() {
+  encoderPos += (digitalRead(encoderPinAR)==digitalRead(encoderPinBR))?1:-1;
+}
+void doEncoderBR(){
+  encoderPos += (digitalRead(encoderPinAR)==digitalRead(encoderPinBR))?-1:1;
+}
+
+void doMotor(bool dir, int vel) {
+  digitalWrite(motorDirPinL, dir);
+  analogWrite(motorPWMPinL, dir?(255 - vel):vel);
+  
+  digitalWrite(motorDirPinR, dir);
+  analogWrite(motorPWMPinR, dir?(255 - vel):vel);
 }
